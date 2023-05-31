ea3bf5d54c126583794304e790570c5ade4743dc
Thao Pham <thao.phamduy@asnet.com.vn>
2023-05-31 18:12:37 +0700

    feat: implement input sotory & create images for header section

---
+++ practices/practice-one/src/stories/Input/index.tsx
@@ -0,0 +1,43 @@
+interface InputProps {
+  value?: string;
+  defaultValue?: string;
+  name?: string;
+  placeholder?: string;
+  isDisabled?: boolean;
+  labelName: string;
+  required?: boolean;
+  search?: boolean;
+  type?: string;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Input = ({
+  defaultValue = '',
+  name = '',
+  placeholder = '',
+  isDisabled = false,
+  labelName = '',
+  type,
+  search = false,
+  required = false,
+}: InputProps) => {
+  const mode = search ? 'input input--search' : 'input input--default';
+  return (
+    <>
+      <label className="input-label">
+        {labelName}
+        {required && <span className="labelRequired">*</span>}
+      </label>
+      <input
+        defaultValue={defaultValue}
+        name={name}
+        className={mode}
+        type={type}
+        placeholder={placeholder}
+        disabled={isDisabled}
+      />
+    </>
+  );
+};
