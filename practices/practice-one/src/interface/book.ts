bc42c9a90aafd2bbcda627b4693d9f3dc6c88d4b
Thao Pham <thao.phamduy@asnet.com.vn>
2023-06-05 18:01:31 +0700

    feat: implement interface & data json books

---
+++ practices/practice-one/src/interface/book.ts
@@ -0,0 +1,10 @@
+export interface IBook {
+  id: number;
+  title: string;
+  category: string;
+  description?: string;
+  image: string;
+  author: string;
+  published: string;
+  Publishers?: string;
+}
