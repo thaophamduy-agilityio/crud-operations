45afb566732f4ca2db721bb9a2c6d223dc7cd21f
Thao Pham <thao.phamduy@asnet.com.vn>
2023-06-07 18:12:28 +0700

    fix: implement list book items & categories

--- practices/practice-one/src/App.tsx
+++ practices/practice-one/src/App.tsx
@@ -1,12 +1,72 @@
 import Header from '@components/Header';
-import Main from '@pages/Books';
+import { useCallback, useEffect, useState } from 'react';
+import { IBook } from './interface/book';
+import endpoint from './helpers/endpoints-config';
+import axios from 'axios';
 import './App.scss';
+import { Card } from '@components/Card';
 
 const App = () => {
+  const [books, setBooks] = useState<IBook[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const { data } = await axios.get<IBook[]>(
+        `${process.env.API_ENDPOINT}/${endpoint.BooksBaseUrl}`
+      );
+      setBooks(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }, []);
+
   return (
     <div className="container">
       <Header />
-      <Main />
+      <main className="main-site">
+        <aside className="column-sidebar">
+          <div className="book-category-title">Categories</div>
+          <div className="book-category-list">A curated list of every book ever written</div>
+          <div className="book-category-wrapper">
+            <ul className="book-category">
+              {books.length > 0 && (
+                <>
+                  {books.map((book) => (
+                    <li className="book-category-item" key={book.id}>
+                      {book.category}
+                    </li>
+                  ))}
+                </>
+              )}
+            </ul>
+          </div>
+        </aside>
+        <section className="column-content">
+          <div className="book-filter-wrapper"></div>
+          <div className="book-list-wrapper">
+            <ul className="book-list">
+              {books.length > 0 && (
+                <>
+                  {books.map((book) => (
+                    <li className="book-item" key={book.id}>
+                      <Card
+                        book={book}
+                        onClick={() => {
+                          /* Handle pop up action */
+                        }}
+                      />
+                    </li>
+                  ))}
+                </>
+              )}
+            </ul>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
--- practices/practice-one/src/pages/Books/index.tsx
+++ practices/practice-one/src/pages/Books/index.tsx
@@ -1,37 +1,32 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { IBook } from '../../interface/book';
 import endpoint from '../../helpers/endpoints-config';
-import { APIRequest } from '../../services';
-import { ERROR_MESSAGES } from '@constants/error-messages';
-import clsx from 'clsx';
+import axios from 'axios';
 
 const Main: FC = () => {
   const [books, setBooks] = useState<IBook[]>([]);
-  const [isBookModal, setIsBookModal] = useState<boolean>(false);
-  const [isOpenMenuSideBar, setIsOpenMenuSideBar] = useState<boolean>(false);
-
-  // Classes
-  const classes = clsx('column-sidebar', { 'column-sidebar-open': isOpenMenuSideBar });
 
   useEffect(() => {
-    const fetchBook = async (): Promise<void> => {
-      try {
-        const listBlogServer = await APIRequest.getAPI(
-          `${import.meta.env.API_ENDPOINT}/${endpoint.BooksBaseUrl}`
-        );
-        setBooks(listBlogServer);
-      } catch (error) {
-        throw new Error(ERROR_MESSAGES.NOT_FOUND);
-      }
-    };
+    fetchData();
+  });
 
-    fetchBook();
+  const fetchData = useCallback(async () => {
+    try {
+      const { data } = await axios.get<IBook[]>(
+        `${process.env.API_ENDPOINT}/${endpoint.BooksBaseUrl}`
+      );
+      setBooks(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }, []);
 
   return (
     <main className="main-site">
       <aside className="column-sidebar">List categories</aside>
-      <section className="column-content">{books.length > 0 && <>List books</>}</section>
+      <section className="column-content">
+        {books.length > 0 && <>List books: {books.length} </>}
+      </section>
     </main>
   );
 };
