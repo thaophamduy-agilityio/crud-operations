30de2cf7c36ab778085e4d1334f2c4ccc4c25ede
Thao Pham <thao.phamduy@asnet.com.vn>
2023-06-17 11:30:07 +0700

    refactor: update liter list by categories

--- practices/practice-one/src/helpers/categories.ts
+++ practices/practice-one/src/helpers/categories.ts
@@ -1,10 +1,5 @@
 import { IBook } from '@interface/book';
-import { CATEGORIES } from '@constants/categories';
 
 export const filterListByCategories = (listBooks: IBook[], categoryName: string) => {
-  if (categoryName) {
-    return listBooks.filter((item) => item?.categoryName === categoryName);
-  } else {
-    return listBooks;
-  }
+  return listBooks.filter((item) => item?.categoryName === categoryName);
 };
