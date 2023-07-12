8fd4fc051ff8f0fca40f36db7f3f89e136eae57e
Thao Pham <thao.phamduy@asnet.com.vn>
2023-07-12 17:34:26 +0700

    fix: remove inline style & update syntax label

--- practices/practice-one/src/components/common/Button/index.tsx
+++ practices/practice-one/src/components/common/Button/index.tsx
@@ -1,6 +1,5 @@
 interface ButtonProps {
   className?: string;
-  backgroundColor?: string;
   size?: 'small' | 'medium' | 'large';
   label?: string;
   text?: string;
@@ -10,23 +9,16 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  size = 'medium',
-  backgroundColor,
-  label,
-  text,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ size = 'medium', label, text, ...props }: ButtonProps) => {
   return (
     <div className="btn-container">
       <button
-        aria-label={`Aria button`}
+        aria-label="Aria button"
         type="button"
         className={['btn', `btn-${size}`].join(' ')}
-        style={{ backgroundColor }}
         {...props}
       >
-        {!!label && `${label}`}
+        {label}
       </button>
       {!!text && <h2 className="text">{text}</h2>}
     </div>
