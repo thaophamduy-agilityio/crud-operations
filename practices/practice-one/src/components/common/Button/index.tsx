d18057922c467894209c55f6ee720c7c20e93d36
Thao Pham <thao.phamduy@asnet.com.vn>
2023-07-24 18:18:24 +0700

    fix: remove div container & text inside button component

--- practices/practice-one/src/components/common/Button/index.tsx
+++ practices/practice-one/src/components/common/Button/index.tsx
@@ -1,26 +1,16 @@
 interface ButtonProps {
-  className?: string;
-  size?: 'small' | 'medium' | 'large';
-  label?: string;
-  text?: string;
+  className: string;
+  label: string;
   onClick: () => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ size = 'medium', label, text, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({ className, label, ...props }: ButtonProps): JSX.Element => {
   return (
-    <div className="btn-container">
-      <button
-        aria-label="Aria button"
-        type="button"
-        className={['btn', `btn-${size}`].join(' ')}
-        {...props}
-      >
-        {label}
-      </button>
-      {!!text && <h2 className="text">{text}</h2>}
-    </div>
+    <button aria-label="Aria button" type="button" className={className} {...props}>
+      {label}
+    </button>
   );
 };
