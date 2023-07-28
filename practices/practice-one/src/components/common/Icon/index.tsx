41441e78fa56f45c417e8f4daa14f751c3031988
Thao Pham <thao.phamduy@asnet.com.vn>
2023-07-28 16:57:56 +0700

    refactor: change div tag to become i tag on icon component

--- practices/practice-one/src/components/common/Icon/index.tsx
+++ practices/practice-one/src/components/common/Icon/index.tsx
@@ -7,5 +7,5 @@ interface IconProps {
  * Primary UI component for user interaction
  */
 export const Icon = ({ className, ...props }: IconProps): JSX.Element => {
-  return <div aria-label="Aria icon" className={className} {...props}></div>;
+  return <i aria-label="Aria icon" className={className} {...props} />;
 };
