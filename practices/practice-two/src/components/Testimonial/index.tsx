287242a2cce9bfc69618ccc66939cdb785a1d66e
Thao Pham <thao.phamduy@asnet.com.vn>
2023-08-11 18:02:22 +0700

    feat: implement component & storybook for testimonial

---
+++ practices/practice-two/src/components/Testimonial/index.tsx
@@ -0,0 +1,47 @@
+import { Testimony } from '@interface/testimonial';
+import { Text } from '@components/common/Text';
+import { memo } from 'react';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+
+// Define the props for the Navigation component
+interface TestimonialProps {
+  testimonials: Testimony[];
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Testimonial = memo(({ testimonials }: TestimonialProps): JSX.Element => {
+  return (
+    <>
+      <div className="testimonial-title">
+        <Text
+          size="xl"
+          text="What people are saying"
+        />
+      </div>
+      <ul className="testimonial">
+        {testimonials.map((testimonial) => (
+          <li key={testimonial.id} id={`testimonial-${testimonial.id}`} className="testimonial-content">
+            <Text
+              size="lg"
+              text={testimonial.desc}
+            />
+            <div className={`star-${testimonial.ratingNumber}`}></div>
+            <div className="testimonial-author">
+              {testimonial.author}
+            </div>
+            <Text
+              size="md"
+              text={testimonial.title}
+            />
+          </li>
+        ))}
+      </ul>
+      <div className="testimonial-icon">
+        <BsArrowLeft className="testimonial-icon-pre disabled" />
+        <BsArrowRight className="testimonial-icon-next" />
+      </div>
+    </>
+  )
+});
