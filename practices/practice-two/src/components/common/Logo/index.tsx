4d196def7e2f2de40b0d65d3f94b25ef367e5f10
Thao Pham <thao.phamduy@asnet.com.vn>
2023-08-14 10:52:36 +0700

    fix: update structure for logo component

--- practices/practice-two/src/components/common/Logo/index.tsx
+++ practices/practice-two/src/components/common/Logo/index.tsx
@@ -4,13 +4,11 @@ import logo from '@image/logo-solo.webp';
 
 const Logo = (): JSX.Element => {
   return (
-    <section className="header-left">
-      <a className="logo" href={ROUTES.HOME} title="Logo Solo">
-        <Image altText="Logo Solo" imageSrc={logo} size= {{width: 33, height: 37}}
-      />
-        <p className="logo-text">Solo <span className="logo-dot">.</span></p>
-      </a>
-    </section>
+    <a className="logo" href={ROUTES.HOME} title="Logo Solo">
+      <Image altText="Logo Solo" imageSrc={logo} size= {{width: 33, height: 37}}
+    />
+      <p className="logo-text">Solo <span className="logo-dot">.</span></p>
+    </a>
   );
 };
 
