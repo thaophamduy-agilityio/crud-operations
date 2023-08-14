c58296d45d53607a46c36a78f2db203d6bfa78c4
Thao Pham <thao.phamduy@asnet.com.vn>
2023-08-14 16:51:43 +0700

    fix: update images path for hero section

--- practices/practice-two/src/constants/images.ts
+++ practices/practice-two/src/constants/images.ts
@@ -1,14 +1,14 @@
 export const heroImg = {
   desktop: {
-    img: '/src/assets/images/hero-desktop.webp',
-    img2x: '/src/assets/images/hero-desktop-2x.webp',
+    img: '/src/assets/images/hero/hero-desktop.webp',
+    img2x: '/src/assets/images/hero/hero-desktop-2x.webp',
   },
   tablet: {
-    img: '/src/assets/images/hero-tablet.webp',
-    img2x: '/src/assets/images/hero-tablet-2x.webp',
+    img: '/src/assets/images/hero/hero-tablet.webp',
+    img2x: '/src/assets/images/hero/hero-tablet-2x.webp',
   },
   mobile: {
-    img: '/src/assets/images/hero-mobile.webp',
-    img2x: '/src/assets/images/hero-mobile-2x.webp',
+    img: '/src/assets/images/hero/hero-mobile.webp',
+    img2x: '/src/assets/images/hero/hero-mobile-2x.webp',
   },
 };
