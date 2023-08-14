0ee95099badcf59a38b3588c9c2bd2573c530413
Thao Pham <thao.phamduy@asnet.com.vn>
2023-08-14 15:58:23 +0700

    feat: implement hero component

---
+++ practices/practice-two/src/components/Hero/index.tsx
@@ -0,0 +1,66 @@
+import { memo } from 'react';
+import { Text } from '@components/common/Text';
+import { Button } from '@components/common/Button';
+import { Image } from '@components/common/Image';
+import { heroImg } from '@constants/images';
+
+// Define the props for the Image component
+interface HeroProps {
+  onTryItFree: () => void;
+  onWatchHowItWorks: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Hero = memo(({ onTryItFree, onWatchHowItWorks }: HeroProps): JSX.Element => {  
+  return (
+    <section className="hero">
+      <div className="hero-typography">
+        <p className="hero-title">Risk-free 30 days trial</p>
+        <Text 
+          size="xxl"
+          text="The best way to organize your work."
+        />
+        <Text 
+          size="md"
+          text="Organize your tasks, lists and reminders in one app."
+        />
+        <div className="btn-container">
+          <Button
+            label="Try it free"
+            onClick={onTryItFree}
+            variant="primary"
+          />
+          <Button
+            label="Watch how it works"
+            isShowLeftButton={true}
+            onClick={onWatchHowItWorks}
+            variant="secondary"
+          />
+        </div>
+      </div>
+      <div className="hero-image">
+        <picture>
+          <source
+            srcSet={`${heroImg.desktop.img2x} 2x, ${heroImg.desktop.img}`}
+            media="(min-width: 1120px)"
+          />
+          <source
+            srcSet={`${heroImg.tablet.img2x} 2x, ${heroImg.tablet.img}`}
+            media="(min-width: 768px)"
+          />
+          <Image
+            srcSet={`${heroImg.mobile.img2x} 2x`}
+            imageSrc={heroImg.mobile.img}
+            size={{
+              height: 376,
+              width: 436,
+            }}
+            altText="Hero"
+          />
+        </picture>
+      </div>
+    </section>
+  )
+});
---
+++ practices/practice-two/src/constants/images.ts
@@ -0,0 +1,14 @@
+export const heroImg = {
+  desktop: {
+    img: '/src/assets/images/hero-desktop.webp',
+    img2x: '/src/assets/images/hero-desktop-2x.webp',
+  },
+  tablet: {
+    img: '/src/assets/images/hero-tablet.webp',
+    img2x: '/src/assets/images/hero-tablet-2x.webp',
+  },
+  mobile: {
+    img: '/src/assets/images/hero-mobile.webp',
+    img2x: '/src/assets/images/hero-mobile-2x.webp',
+  },
+};
