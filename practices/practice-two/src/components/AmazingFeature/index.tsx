155cc7a4ac23ea242d3031313195ce12003e284f
Thao Pham <thao.phamduy@asnet.com.vn>
2023-08-21 14:39:56 +0700

    fix: split import group by libs, components & constants and check default value for amazing feature

--- practices/practice-two/src/components/AmazingFeature/index.tsx
+++ practices/practice-two/src/components/AmazingFeature/index.tsx
@@ -1,9 +1,14 @@
-import { Feature } from '@interface/amazing-feature';
+// Libs
 import { memo } from 'react';
+
+// Components
+import { Feature } from '@interface/amazing-feature';
 import { Image } from '@components/common/Image';
 import { Text } from '@components/common/Text';
 import { Button } from '@components/common/Button';
-import { amazingFeatureImg } from '@constants/images';
+
+// Constants
+import { AmazingFeatureImg } from '@constants/images';
 
 // Define the props for the Amazing Feature component
 interface AmazingFeatureProps {
@@ -13,21 +18,105 @@ interface AmazingFeatureProps {
 /**
  * Primary UI component for user interaction
  */
-export const AmazingFeature = memo(({ features }: AmazingFeatureProps): JSX.Element => {
+export const AmazingFeature = memo(({ 
+  features = [
+    {
+      id: "1",
+      srcSet: {
+        desktop: {
+          img: "@images/amazing-feature/feature-how-it-work-desktop.webp",
+          img2x: "@images/amazing-feature/feature-how-it-work-desktop-2x.webp",
+        },
+        tablet: {
+          img: "@images/amazing-feature/feature-how-it-work-tablet.webp",
+          img2x: "@images/amazing-feature/feature-how-it-work-tablet-2x.webp",
+        },
+      },
+      imageSrc: {
+        mobile: {
+          img: "@images/amazing-feature/feature-how-it-work-mobile.webp",
+          img2x: "@images/amazing-feature/feature-how-it-work-mobile-2x.webp",
+        },
+      },
+      iconSrc: {
+        img: "@images/amazing-feature/feature-how-it-work-icon.webp",
+        img2x: "@images/amazing-feature/feature-how-it-work-icon-2x.webp",
+      },
+      title: "Stay focused whenever, wherever",
+      desc: "We're a growing family of 382,081 designers and makers from around the world",
+      btnLabel: "Watch how it work",
+      onClick: () => {},
+    },
+    {
+      id: "2",
+      srcSet: {
+        desktop: {
+          img: "@images/amazing-feature/feature-get-app-desktop.webp",
+          img2x: "@images/amazing-feature/feature-get-app-desktop-2x.webp",
+        },
+        tablet: {
+          img: "@images/amazing-feature/feature-get-app-tablet.webp",
+          img2x: "@images/amazing-feature/feature-get-app-tablet-2x.webp",
+        },
+      },
+      imageSrc: {
+        mobile: {
+          img: "@images/amazing-feature/feature-get-app-mobile.webp",
+          img2x: "@images/amazing-feature/feature-get-app-mobile-2x.webp",
+        },
+      },
+      iconSrc: {
+        img: "@images/amazing-feature/feature-get-app-icon.webp",
+        img2x: "@images/amazing-feature/feature-get-app-icon-2x.webp",
+      },
+      title: "Share files at the right time",
+      desc: "We're a growing family of 382,081 designers and makers from around the world",
+      btnLabel: "Get Solo Desktop",
+      onClick: () => {},
+    },
+    {
+      id: "3",
+      srcSet: {
+        desktop: {
+          img: "@images/amazing-feature/feature-schedule-demo-desktop.webp",
+          img2x: "@images/amazing-feature/feature-schedule-demo-desktop-2x.webp",
+        },
+        tablet: {
+          img: "@images/amazing-feature/feature-schedule-demo-tablet.webp",
+          img2x: "@images/amazing-feature/feature-schedule-demo-tablet-2x.webp",
+        },
+      },
+      imageSrc: {
+        mobile: {
+          img: "@images/amazing-feature/feature-schedule-demo-mobile.webp",
+          img2x: "@images/amazing-feature/feature-schedule-demo-mobile-2x.webp",
+        },
+      },
+      iconSrc: {
+        img: "@images/amazing-feature/feature-schedule-demo-icon.webp",
+        img2x: "@images/amazing-feature/feature-schedule-demo-icon-2x.webp",
+      },
+      title: "Collaborate from anywhere",
+      desc: "We're a growing family of 382,081 designers and makers from around the world",
+      btnLabel: "Schedule a demo",
+      onClick: () => {},
+    },
+  ]
+}: AmazingFeatureProps): JSX.Element => {
   return (
     <section className="amazing-feature">
       <picture className="amazing-feature-image">
         <source
-          srcSet={`${amazingFeatureImg.desktop.img2x} 2x, ${amazingFeatureImg.desktop.img}`}
+          srcSet={`${AmazingFeatureImg.desktop.img2x} 2x, ${AmazingFeatureImg.desktop.img}`}
           media="(min-width: 1120px)"
         />
         <source
-          srcSet={`${amazingFeatureImg.tablet.img2x} 2x, ${amazingFeatureImg.tablet.img}`}
+          srcSet={`${AmazingFeatureImg.tablet.img2x} 2x, ${AmazingFeatureImg.tablet.img}`}
           media="(min-width: 768px)"
         />
         <Image
-          srcSet={`${amazingFeatureImg.mobile.img2x} 2x`}
-          imageSrc={amazingFeatureImg.mobile.img}
+          srcSet={`${AmazingFeatureImg.mobile.img2x} 2x`}
+          imageSrc={AmazingFeatureImg.mobile.img}
           size={{
             height: 186,
             width: 423,
