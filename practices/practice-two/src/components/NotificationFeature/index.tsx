77ca142b41799535ef795e43b34a968225c78c01
Thao Pham <thao.phamduy@asnet.com.vn>
2023-08-23 16:58:27 +0700

    fix: change the way to show content in notification feature component

--- practices/practice-two/src/components/NotificationFeature/index.tsx
+++ practices/practice-two/src/components/NotificationFeature/index.tsx
@@ -2,12 +2,7 @@
 import { memo } from 'react';
 
 // Components
-import { Text } from '@components/common/Text';
-import { Button } from '@components/common/Button';
-import { Image } from '@components/common/Image';
-
-// Constants
-import { NotificationImg } from '@constants/images';
+import { FeatureItem } from '@components/common/FeatureItem';
 
 // Define the props for the Notification Feature component
 interface NotificationFeatureProps {
@@ -19,44 +14,33 @@ interface NotificationFeatureProps {
  */
 export const NotificationFeature = memo(({ onFindOut }: NotificationFeatureProps): JSX.Element => {
   return (
-    <section className="notification">
-      <div className="notification-content">
-        <p className="notification-title">Amazing features</p>
-        <Text 
-          size="xl"
-          text="Never forget anything, ever again"
-        />
-        <Text 
-          size="md"
-          text="We're a growing family of 382,081 designers and makers from around the world"
-        />
-        <div className="btn-container">
-          <Button
-            label="Find out more"
-            onClick={onFindOut}
-            variant="secondary"
-          />
-        </div>
-      </div>
-      <picture className="notification-image">
-        <source
-          srcSet={`${NotificationImg.desktop.img2x} 2x, ${NotificationImg.desktop.img}`}
-          media="(min-width: 1120px)"
-        />
-        <source
-          srcSet={`${NotificationImg.tablet.img2x} 2x, ${NotificationImg.tablet.img}`}
-          media="(min-width: 768px)"
-        />
-        <Image
-          srcSet={`${NotificationImg.mobile.img2x} 2x`}
-          imageSrc={NotificationImg.mobile.img}
-          size={{
-            height: 345,
-            width: 480,
-          }}
-          altText="Launch app feature"
-        />
-      </picture>
-    </section>
+    <FeatureItem 
+      classItem="notification"
+      srcSet = {{
+        desktop: {
+          img: '/src/assets/images/notification/notification-desktop.webp',
+          img2x: '/src/assets/images/notification/notification-desktop-2x.webp',
+        },
+        tablet: {
+          img: '/src/assets/images/notification/notification-tablet.webp',
+          img2x: '/src/assets/images/notification/notification-tablet-2x.webp',
+        },
+      }}
+      imageSrc = {{
+        mobile: {
+          img: '/src/assets/images/notification/notification-mobile.webp',
+          img2x: '/src/assets/images/notification/notification-mobile-2x.webp',
+        }
+      }}
+      imageSize = {{
+        width:684,
+        height:487
+      }}
+      subTitle="Amazing features"
+      title="Never forget anything, ever again"
+      desc="We're a growing family of 382,081 designers and makers from around the world"
+      labelButton="Find out more"
+      onClick={onFindOut}
+    />
   )
 });
