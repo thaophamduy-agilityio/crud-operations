783a842e93adc834929d2df9d4bb3e13b6796490
Thao Pham <thao.phamduy@asnet.com.vn>
2023-09-13 09:09:00 +0700

    feat: implement testing for book item component

--- practices/practice-one/src/components/BookItem/index.tsx
+++ practices/practice-one/src/components/BookItem/index.tsx
@@ -1,5 +1,5 @@
 import { IBook } from '@interface/book';
-import { Image } from '@components/common/Image';
+import { Image } from 'src/components/common/Image';
 import { memo } from 'react';
 
 interface BookProps {
@@ -17,22 +17,20 @@ const BookItem = ({ width, book, isDisplayBooks, toggleModal }: BookProps): JSX.
   };
 
   return (
-    <li className={`book-item ${isDisplayBooks ? 'list' : ''}`}>
-      <div onClick={onToggleModal}>
-        <figure className="card-container">
-          <div className="card-container-img">
-            <picture>
-              <source className="img-item" srcSet={imageMedium} media="(min-width: 768px)" />
-              <Image loading="lazy" imageSrc={imageSmall} width={width} altText={title} />
-            </picture>
-          </div>
-          <figcaption>
-            <h2 className="card-title">{title}</h2>
-            <p className="card-author">{author}</p>
-            <p className="card-published">{published}</p>
-          </figcaption>
-        </figure>
-      </div>
+    <li onClick={onToggleModal} className={`book-item ${isDisplayBooks ? 'list' : ''}`}>
+      <figure className="card-container">
+        <div className="card-container-img">
+          <picture>
+            <source className="img-item" srcSet={imageMedium} media="(min-width: 768px)" />
+            <Image loading="lazy" imageSrc={imageSmall} width={width} altText={title} />
+          </picture>
+        </div>
+        <figcaption>
+          <h2 className="card-title">{title}</h2>
+          <p className="card-author">{author}</p>
+          <p className="card-published">{published}</p>
+        </figcaption>
+      </figure>
     </li>
   );
 };
