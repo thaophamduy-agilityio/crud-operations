dd071af2e336b688c3f34a8429a4abe1be12adac
Thao Pham <thao.phamduy@asnet.com.vn>
2023-09-20 17:44:19 +0700

    fix: update testing for bread crumb component

--- practices/practice-one/src/components/BreadCrumb/__test__/index.test.tsx
+++ practices/practice-one/src/components/BreadCrumb/__test__/index.test.tsx
@@ -1,5 +1,5 @@
 // libs
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 // components
@@ -10,6 +10,34 @@ describe('testing bread crumb component', () => {
     selectedCategory: 'Adventure',
     total: 18,
   };
+
+  it('should render correctly with display selected category', () => {
+    const { container } = render(<BreadCrumb {...propsDefault} />);
+    const breadCrumb = container.getElementsByClassName('book-title')[0];
+    const selectedCategory = container.getElementsByClassName('book-title-text')[0];
+    const results = screen.getByTestId('bread crumb results');
+
+    expect(breadCrumb).toBeInTheDocument();
+    expect(selectedCategory.textContent).toBe(propsDefault.selectedCategory);
+    expect(results).toHaveTextContent('Showing 18 Results');
+  });
+
+  it('should render correctly with total item in category', () => {
+    const { container } = render(<BreadCrumb {...propsDefault} />);
+    const breadCrumb = container.getElementsByClassName('book-title')[0];
+
+    expect(breadCrumb.firstChild).toHaveClass('book-title-text');
+    expect(propsDefault.total).toEqual(18);
+  });
+
+  it('should render correctly show result item', () => {
+    const { container } = render(<BreadCrumb {...propsDefault} total={1} />);
+    const breadCrumb = container.getElementsByClassName('book-title')[0];
+    const results = screen.getByTestId('bread crumb results');
+
+    expect(breadCrumb).toHaveClass('book-title');
+    expect(results).toHaveTextContent('Showing 1 Result');
+  });
 
   it('should be render match to snapshot', () => {
     const component = render(<BreadCrumb {...propsDefault} />);
--- practices/practice-one/src/components/BreadCrumb/index.tsx
+++ practices/practice-one/src/components/BreadCrumb/index.tsx
@@ -9,12 +9,14 @@ interface BreadCrumbProps {
 
 const BreadCrumb = ({ selectedCategory, total = 0 }: BreadCrumbProps): JSX.Element => {
   return (
-    <div className="book-title">
-      <span className="book-title-text">{selectedCategory || 'All Books'}</span>
-      <span className="book-title-arrow">
+    <div className="book-title" data-testid="bread crumb">
+      <span className="book-title-text" data-testid="bread crumb selected category">
+        {selectedCategory || 'All Books'}
+      </span>
+      <span className="book-title-arrow" data-testid="bread crumb arrow">
         <Image altText="arrow" imageSrc={arrow} loading="lazy" width={22} />
       </span>
-      <span className="book-title-results">
+      <span className="book-title-results" data-testid="bread crumb results">
         Showing {total} Result{total > 1 ? 's' : ''}
       </span>
     </div>
