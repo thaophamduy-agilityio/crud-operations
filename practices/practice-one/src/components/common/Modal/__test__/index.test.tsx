ca959735fb97247669a211eea8f009c5b428f38f
Thao Pham <thao.phamduy@asnet.com.vn>
2023-09-27 17:06:24 +0700

    fix: update testing for modal component

--- practices/practice-one/src/components/common/Modal/__test__/index.test.tsx
+++ practices/practice-one/src/components/common/Modal/__test__/index.test.tsx
@@ -1,5 +1,5 @@
 // libs
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 // components
@@ -17,22 +17,48 @@ describe('testing modal component', () => {
 
   it('should be call handle click icon close modal', () => {
     const { container } = render(<Modal {...propsDefault} />);
-
     const iconCloseModal = container.getElementsByClassName('icon-close-modal')[0];
+    const modalOverlay = container.getElementsByClassName('modal-overlay')[0];
 
     fireEvent.click(iconCloseModal);
 
     expect(propsDefault.onCloseModal).toBeCalledTimes(1);
+
+    fireEvent.click(modalOverlay);
+
+    expect(propsDefault.onCloseModal).toBeCalledTimes(2);
+  });
+
+  it('testing component modal render when isShowModal is true', () => {
+    const { container } = render(<Modal {...propsDefault} />);
+    const title = screen.getByText('Angels and demons');
+    const modalOverlay = container.getElementsByClassName('modal-overlay')[0];
+
+    expect(title).toBeInTheDocument();
+    expect(modalOverlay).toBeInTheDocument();
+  });
+
+  it('testing component modal not render when isShowModal is false', () => {
+    const container = render(
+      <Modal {...propsDefault} isShowModal={false} children="<h2>This is modal</h2>" />
+    );
+    const heading = document.querySelector('h2');
+
+    expect(container).toMatchSnapshot();
+    expect(heading).not.toBeInTheDocument();
   });
 
   it('should be call handle click icon change dark theme', () => {
     const { container } = render(<Modal {...propsDefault} />);
-
     const iconChangeDarkTheme = container.getElementsByClassName('icon-sunshine-modal')[0];
+    const modal = container.getElementsByClassName('modal')[0];
 
     fireEvent.click(iconChangeDarkTheme);
 
     expect(propsDefault.onToggleThemeModal).toBeCalledTimes(1);
+    expect(iconChangeDarkTheme).toHaveClass('dark');
+    expect(modal).toBeInTheDocument();
+    expect(modal).toHaveClass('dark-theme');
   });
 
   it('should be render match to snapshot', () => {
