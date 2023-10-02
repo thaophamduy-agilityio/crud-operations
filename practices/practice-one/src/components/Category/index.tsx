69c890ba01a413e77c33f593515a0c4b18c19d07
Thao Pham <thao.phamduy@asnet.com.vn>
2023-10-02 10:15:21 +0700

    fix: update category component

--- practices/practice-one/src/components/Category/index.tsx
+++ practices/practice-one/src/components/Category/index.tsx
@@ -8,29 +8,27 @@ interface CategoryProps {
 }
 
 const Category = ({ category, categorySelected, onSelectCategory }: CategoryProps): JSX.Element => {
-  const { id = 'all', name = 'All books', total = 18 } = category || {};
-
   const handleSelectCategory = (): void => {
-    onSelectCategory(name);
+    onSelectCategory(category.name);
   };
 
   return (
     <li
-      className={`book-category-item ${name === categorySelected ? 'selected' : ''}`}
-      data-id={id}
+      className={`book-category-item ${category.name === categorySelected ? 'selected' : ''}`}
+      data-id={category.id}
       onClick={handleSelectCategory}
       data-testid="category"
     >
       <div className="book-category-name">
-        <span className={['book-category-shorthand', `book-category-${name}`].join(' ')}>
-          {name.substring(0, 2)}
+        <span className={['book-category-shorthand', `book-category-${category.name}`].join(' ')}>
+          {category.name.substring(0, 2)}
         </span>
         <span className="name" data-testid="category name">
-          {name}
+          {category.name}
         </span>
       </div>
       <div className="book-category-total" data-testid="category total">
-        {total}
+        {category.total}
       </div>
     </li>
   );
