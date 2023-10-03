68513a5dca75edb07ed9ee9c425515386ba54fe1
Thao Pham <thao.phamduy@asnet.com.vn>
2023-10-03 09:52:22 +0700

    fix: update filter sort component for testing

--- practices/practice-one/src/components/FilterSort/index.tsx
+++ practices/practice-one/src/components/FilterSort/index.tsx
@@ -10,7 +10,7 @@ interface FilterSortProps {
 }
 
 const FilterSort = ({ onSortBooks, sortOption }: FilterSortProps): JSX.Element => {
-  const { isSortByAlphabet, isSortByYear } = sortOption || {};
+  const { isSortByAlphabet, isSortByYear } = sortOption;
 
   return (
     <div className="filter-sort">
