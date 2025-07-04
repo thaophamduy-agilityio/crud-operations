e70a88b9fd01addb63e85927a94c53c0f97f642c
Thao Pham <pham.thao@zenbusiness.com>
2025-07-04 10:17:30 +0000

    Build source base, layout for students dashboard page

---
+++ crud-operations/src/constants/error-messages.ts
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGES = {
+  NOT_FOUND: 'Not found, please try again',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error, please try again',
+};
+
+export const STUDENTS_MESSAGES = {
+  NO_DATA: 'Not found data!',
+};
