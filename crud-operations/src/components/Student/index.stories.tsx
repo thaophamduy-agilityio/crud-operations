3001184211efac0cc56f7c4f77aabe390a97efdf
Thao Pham <pham.thao@zenbusiness.com>
2025-07-14 09:44:44 +0000

    feat: implement student component

---
+++ crud-operations/src/components/Student/index.stories.tsx
@@ -0,0 +1,43 @@
+// Libs
+import type { Meta, StoryObj } from "@storybook/react";
+
+// Components
+import { Student } from "./";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Components/Common/Student',
+  component: Student,
+  tags: ['autodocs'],
+  argTypes: {
+    student: { description: "Student object containing all student details" },
+    onEditItem: { description: "call edit student" },
+    onDeleteItem: { description: "call delete student" },
+  },
+
+} satisfies Meta<typeof Student>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        student: {
+            "id": 1,
+            "firstName": "Karthi",
+            "lastName": "Madesh",
+            "avatar": "https://i.pravatar.cc/150?img=12",
+            "email": "karthi@gmmail.com",
+            "phone": 7305477760,
+            "enrollNumber": 1234567305477760,
+            "dateAdmission": "08-Dec, 2021",
+            "role": "Admin"
+        },
+        onEditItem: () => {
+            console.log('handle edit logic here')
+        },
+        onDeleteItem: () => {
+            console.log('handle delete logic here')
+        },
+    },
+};
---
+++ crud-operations/src/components/Student/index.tsx
@@ -0,0 +1,48 @@
+// Libs
+import type { JSX } from "react";
+
+// Components
+import { IconButton } from "@components/IconButton";
+import { EditIcon, DeleteIcon } from "@components/Icon";
+
+//Interfaces
+import { IStudent } from "@interface/student";
+interface StudentProps {
+    student: IStudent;
+    onEditItem: () => void;
+    onDeleteItem: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Student = ({ student, onEditItem, onDeleteItem }: StudentProps): JSX.Element => {
+    return (
+        <div className="student-item">
+            <div className="student-item-avatar">
+                <img src={student.avatar} alt={student.firstName} width={65} height={65} />
+            </div>
+            <div className="student-item-name">{student.firstName}</div>
+            <div className="student-item-email">{student.email}</div>
+            <div className="student-item-phone">{student.phone}</div>
+            <div className="student-item-enroll-number">{student.enrollNumber}</div>
+            <div className="student-item-date-admission">{student.dateAdmission}</div>
+            <div className="student-item-edit">
+                <IconButton
+                    additionalClasses="icon-edit"
+                    onClick={onEditItem}
+                >
+                    <EditIcon />
+                </IconButton>
+            </div>         
+            <div className="student-item-delete">
+                <IconButton
+                    additionalClasses="icon-delete"
+                    onClick={onDeleteItem}
+                >
+                    <DeleteIcon />
+                </IconButton>
+            </div>
+        </div>
+    );
+}
