5ea6604c981ae8734c9b873be50c707e995222c6
Thao Pham <thao.phamduy@asnet.com.vn>
2025-08-01 10:21:45 +0000

    refactor: update tag for each of part student item

--- crud-operations/src/components/Students/StudentItem/index.tsx
+++ crud-operations/src/components/Students/StudentItem/index.tsx
@@ -5,50 +5,53 @@ import type { JSX } from "react";
 import { IconButton } from "@components/index";
 import { EditIcon, DeleteIcon } from "@components/Icon";
 
-//Interfaces
+// Interfaces
 import { IStudent } from "@interface/student";
 interface StudentProps {
-    selectedStudent: IStudent;
-    onEditItem: (item: IStudent) => void;
-    onDeleteItem: (item: IStudent) => void;
+  selectedStudent: IStudent;
+  onEditItem: (item: IStudent) => void;
+  onDeleteItem: (item: IStudent) => void;
 }
 
 const StudentItem = ({ selectedStudent, onEditItem, onDeleteItem }: StudentProps): JSX.Element => {
-    const handleEditStudent = () => {
-        onEditItem(selectedStudent);
-    }
-    const handleDeleteStudent = () => {
-        onDeleteItem(selectedStudent);
-    }
-    
-    return (
-        <li className="student-item">
-            <div className="student-item-avatar">
-                <img src={selectedStudent.avatar} alt={selectedStudent.firstName} width={65} height={65} />
-            </div>
-            <div className="student-item-name">{selectedStudent.firstName}</div>
-            <div className="student-item-email">{selectedStudent.email}</div>
-            <div className="student-item-phone">{selectedStudent.phone}</div>
-            <div className="student-item-enroll-number">{selectedStudent.enrollNumber}</div>
-            <div className="student-item-date-admission">{selectedStudent.dateAdmission}</div>
-            <div className="student-item-edit">
-                <IconButton
-                    additionalClasses="icon-edit"
-                    onClick={handleEditStudent}
-                >
-                    <EditIcon />
-                </IconButton>
-            </div>         
-            <div className="student-item-delete">
-                <IconButton
-                    additionalClasses="icon-delete"
-                    onClick={handleDeleteStudent}
-                >
-                    <DeleteIcon />
-                </IconButton>
-            </div>
-        </li>
-    );
+  const handleEditStudent = () => {
+    onEditItem(selectedStudent);
+  }
+  const handleDeleteStudent = () => {
+    onDeleteItem(selectedStudent);
+  }
+  
+  return (
+    <li className="student-item">
+      <figure className="student-item-avatar">
+        <img src={selectedStudent.avatar} alt={selectedStudent.firstName} width={65} height={65} />
+      </figure>
+      
+      <h3 className="student-item-name">{selectedStudent.firstName}</h3>
+      
+      <p className="student-item-email">{selectedStudent.email}</p>
+      
+      <span className="student-item-phone">{selectedStudent.phone}</span>
+      
+      <span className="student-item-enroll-number">{selectedStudent.enrollNumber}</span>
+      
+      <p className="student-item-date-admission">{selectedStudent.dateAdmission}</p>
+      
+      <IconButton
+        additionalClasses="student-item-edit icon-edit"
+        onClick={handleEditStudent}
+      >
+        <EditIcon />
+      </IconButton>
+      
+      <IconButton
+        additionalClasses="student-item-delete icon-delete"
+        onClick={handleDeleteStudent}
+      >
+        <DeleteIcon />
+      </IconButton>
+    </li>
+  );
 }
 
 export default StudentItem;
